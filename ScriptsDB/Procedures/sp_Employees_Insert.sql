44edadf0b32f97e44a066e988627b720776cbca9
anthonyFlores123 <misuripingextreme@gmail.com>
2022-02-14 09:17:34 -0500

    correccion
    correcion final

--- ScriptsDB/Procedures/sp_Employees_Insert.sql
+++ ScriptsDB/Procedures/sp_Employees_Insert.sql
@@ -1,48 +1,41 @@
-DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_Employees_Insert`(
-In AtLastName VARCHAR(20),
-In AtFirstName VARCHAR(10),
-In AtTitle VARCHAR(30),
-In AtTitleOfCourtesy VARCHAR(25),
-In AtBirthDate DateTime,
-In AtHireDate DateTime,
-In AtAddress VARCHAR(60),
-In AtCity VARCHAR(15),
-In AtRegion VARCHAR(15),
-In AtPostalCode VARCHAR(10),
-In AtCountry VARCHAR(15),
-In AtHomePhone VARCHAR(24),
-In AtExtension VARCHAR(4),
-In AtPhoto LONGBLOB,
-In AtNotes MEDIUMTEXT,
-In AtReportsTo INTEGER,
-IN AtPhotoPath VARCHAR(255),
-In AtSalary decimal
+CREATE PROCEDURE sp_Employees_Insert (
+@AtLastName VARCHAR(20),
+@AtFirstName VARCHAR(10),
+@AtTitle VARCHAR(30),
+@AtTitleOfCourtesy VARCHAR(25),
+@AtAddress VARCHAR(60),
+@AtCity VARCHAR(15),
+@AtRegion VARCHAR(15),
+@AtPostalCode VARCHAR(10),
+@AtCountry VARCHAR(15),
+@AtHomePhone VARCHAR(24),
+@AtExtension VARCHAR(4),
+@AtNotes VARCHAR(200),
+@AtReportsTo INTEGER,
+@AtPhotoPath VARCHAR(255),
+@AtSalary decimal
 )
+as
 BEGIN
-Insert Into Employees 
+Insert Into northwind.Employees 
 (
-   `LastName`  ,     
-   `FirstName`  ,    
-   `Title`      ,     
-   `TitleOfCourtesy` ,
-   `BirthDate`  ,            
-   `HireDate`  ,    
-   `Address`   ,      
-   `City`    ,        
-   `Region`  ,        
-   `PostalCode`  ,    
-   `Country`        , 
-   `HomePhone`  ,     
-   `Extension`  ,     
-   `Photo`     ,                          
-   `Notes`    ,       
-   `ReportsTo`   ,    
-   `PhotoPath` ,      
-   `Salary`          
+   LastName  ,     
+   FirstName  ,    
+   Title      ,     
+  TitleOfCourtesy ,  
+   Address   ,      
+   City    ,        
+   Region  ,        
+   PostalCode  ,    
+   Country       , 
+   HomePhone  ,     
+   Extension  ,                              
+   Notes    ,       
+   ReportsTo   ,    
+   PhotoPath ,      
+   Salary          
 )
 Values
-(AtLastName,AtFirstName,AtTitle,AtTitleOfCourtesy,AtBirthDate,AtHireDate,AtAddress,AtCity,AtRegion,AtPostalCode,AtCountry,AtHomePhone,AtExtension,AtPhoto,AtNotes,AtReportsTo,AtPhotoPath,AtSalary);
+(@AtLastName,@AtFirstName,@AtTitle,@AtTitleOfCourtesy,@AtAddress,@AtCity,@AtRegion,@AtPostalCode,@AtCountry,@AtHomePhone,@AtExtension,@AtNotes,@AtReportsTo,@AtPhotoPath,@AtSalary);
 	
-END$$
-DELIMITER ;
+END
--- ScriptsDB/Procedures/sp_Employees_Update.sql
+++ ScriptsDB/Procedures/sp_Employees_Update.sql
@@ -1,46 +1,37 @@
-DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_Employees_Update`(
-In AtEmployeeID INTEGER,
-In AtLastName VARCHAR(20),
-In AtFirstName VARCHAR(10),
-In AtTitle VARCHAR(30),
-In AtTitleOfCourtesy VARCHAR(25),
-In AtBirthDate DateTime,
-In AtHireDate DateTime,
-In AtAddress VARCHAR(60),
-In AtCity VARCHAR(15),
-In AtRegion VARCHAR(15),
-In AtPostalCode VARCHAR(10),
-In AtCountry VARCHAR(15),
-In AtHomePhone VARCHAR(24),
-In AtExtension VARCHAR(4),
-In AtPhoto LONGBLOB,
-In AtNotes MEDIUMTEXT,
-In AtReportsTo INTEGER,
-IN AtPhotoPath VARCHAR(255)
+CREATE  PROCEDURE sp_Employees_Update(
+@AtEmployeeID INTEGER,
+@AtLastName VARCHAR(20),
+@AtFirstName VARCHAR(10),
+@AtTitle VARCHAR(30),
+@AtTitleOfCourtesy VARCHAR(25),
+@AtAddress VARCHAR(60),
+@AtCity VARCHAR(15),
+@AtRegion VARCHAR(15),
+@AtPostalCode VARCHAR(10),
+@AtCountry VARCHAR(15),
+@AtHomePhone VARCHAR(24),
+@AtExtension VARCHAR(4),
+@AtReportsTo INTEGER,
+@AtPhotoPath VARCHAR(255)
 )
+as
 BEGIN
-Update Employees
+Update northwind.Employees
 	Set
-		LastName = AtLastName,
-		FirstName = AtFirstName,
-		Title = AtTitle,
-		TitleOfCourtesy = AtTitleOfCourtesy,
-		BirthDate = AtBirthDate,
-		HireDate = AtHireDate,
-		Address = AtAddress,
-		City = AtCity,
-		Region = AtRegion,
-		PostalCode = AtPostalCode,
-		Country = AtCountry,
-		HomePhone = AtHomePhone,
-		Extension = AtExtension,
-		Photo = AtPhoto,
-		Notes = AtNotes,
-		ReportsTo = AtReportsTo,
-    PhotoPath = AtPhotoPath
+		LastName = @AtLastName,
+		FirstName = @AtFirstName,
+		Title = @AtTitle,
+		TitleOfCourtesy = @AtTitleOfCourtesy,
+		Address = @AtAddress,
+		City = @AtCity,
+		Region = @AtRegion,
+		PostalCode = @AtPostalCode,
+		Country = @AtCountry,
+		HomePhone = @AtHomePhone,
+		Extension = @AtExtension,
+		ReportsTo = @AtReportsTo,
+    PhotoPath = @AtPhotoPath
 	Where
-		EmployeeID = AtEmployeeID;
+		EmployeeID = @AtEmployeeID;
 
-END$$
-DELIMITER ;
+END
